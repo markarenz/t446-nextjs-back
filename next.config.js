290da9e9e1d7c18ff61fa981448a992f20db8e3a
Mark Arenz <arenz.mark@gmail.com>
2022-11-06 11:39:39 -0500

    update nextconfig to add env

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    MONGODB_URI: process.env.MONGODB_URI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   }
 };
 
